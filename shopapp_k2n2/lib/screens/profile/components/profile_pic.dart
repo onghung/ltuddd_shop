f555531f18e5cd8d4ef2e7ef27d5b68be086f866
onghung <93970366+onghung@users.noreply.github.com>
2024-01-19 22:29:31 +0700

    Trần Công-GD1
    1, Update giao diện tĩnh profile.
    2, Hiển thị Email người dùng tại trang profile(lấy từ csdl)

---
+++ shopapp_k2n2/lib/screens/profile/components/profile_pic.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ProfilePic extends StatelessWidget {
+  const ProfilePic({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 115,
+      width: 115,
+      child: Stack(
+        fit: StackFit.expand,
+        clipBehavior: Clip.none,
+        children: [
+          SvgPicture.asset("assets/icons/User Icon.svg"),
+          Positioned(
+            right: -16,
+            bottom: 0,
+            child: SizedBox(
+              height: 46,
+              width: 46,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white, shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    side: const BorderSide(color: Colors.white),
+                  ),
+                  backgroundColor: const Color(0xFFF5F6F9),
+                ),
+                onPressed: () {},
+                child: SvgPicture.asset("assets/icons/Camera Icon.svg"),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
