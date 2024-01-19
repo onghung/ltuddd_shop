cc512bad4ebd920df425c16d0c5288d21918c76d
onghung <93970366+onghung@users.noreply.github.com>
2024-01-19 20:14:33 +0700

    Trần Công - first update
    1. Khung ứng dụng
    2. Tích hợp csdl Firebase
    3. Hoàn thành chức năng đăng nhập
    4. Giao diện đăng ký, quên mật khẩu
    5. Trang Home: giao diện tĩnh demo, form navigation bar

---
+++ shopapp_k2n2/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:untitled15/screens/splash/splash_screen.dart';
+
+import 'routes.dart';
+import 'theme.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp( const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Project lập trình ứng dụng di động K2N2',
+      theme: AppTheme.lightTheme(context),
+      initialRoute: SplashScreen.routeName,
+      routes: routes,
+    );
+  }
+}
